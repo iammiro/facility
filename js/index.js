32d73c677bcc4b7bbe35a9c7dee282a632673897
iammiro <mir.kolomiets@gmail.com>
2017-11-20 15:43:24 +0200

    Update 2017-11-20T13:43:02.307Z

--- js/index.js
+++ js/index.js
@@ -1,16 +1,43 @@
 $(document).ready(function () {
+
+    $(function () {
+        $('#menu').slicknav();
+    });
+
     $('.big-slider').slick({
+        dots: true,
         prevArrow: $('.prev'),
         nextArrow: $('.next'),
-        dots: true
     });
 
     $('.little-slider').slick({
-        infinite: true,
+        speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
         prevArrow: $('.prev-little'),
-        nextArrow: $('.next-little')
+        nextArrow: $('.next-little'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
     });
 
     $("#menu").on("click", "a", function (event) {
